a8254fa244807fa6692d8f26c7fb483dca73534b
elise <rousseaujumelle@gmail.com>
2021-02-09 16:20:43 +0100

    setting up router

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link> 
+      <router-link to="/contact">Contact us</router-link>
+    </div> -->
+    <Navbar/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Nav'
+
+export default {
+  components: { Navbar}
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Livvic:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,900&display=swap');
+body {
+  margin: 0;
+}
+#app {
+  font-family: 'Livvic', sans-serif;
+  //colors primary
+  $midnightGreen: #014E56;
+  $lightCoral: #F67E7E;
+  $white: #FFF;
+
+  //colors secondary 
+  $raptureBlue: #79C8C7;
+  $policeBlue: #2C6269;
+  $deepJungleGreen: #004047;
+  $sacramentoStateGreen: #012F34;
+  $darkGreen: #002529;
+
+  .btn {
+    width: 153px;
+    height: 48px;
+    border-radius: 24px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .normal-state-light {
+    border: 2px solid #FFFFFF;
+    box-sizing: border-box;
+  }
+
+
+
+  // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  // color: #2c3e50;
+}
+
+// #nav {
+//   padding: 30px;
+
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
+</style>
